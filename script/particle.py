6cc667468d96763fff419e2fb5ca5a31d38a07c1
kazumakano <kazumakano@outlook.jp>
2022-01-10 19:20:39 +0900

    minor modify

--- script/particle.py
+++ script/particle.py
@@ -7,12 +7,6 @@ import particle_filter.script.utility as pf_util
 
 
 class Particle(MmParticle):
-    def walk(self, angle: np.float64, vec: np.ndarray) -> None:
-        if param.ENABLE_PDR_WALK:
-            self._walk(angle + np.random.normal(scale=pf_param.DIRECT_SD), np.linalg.norm(vec) + np.random.normal(scale=param.STRIDE_SD))
-        else:
-            super().random_walk()
-
     def set_likelihood(self, is_on_corner: bool, last_pos: np.ndarray, map: Map, strength_weight_list: np.ndarray, subject_dist_list: np.ndarray) -> None:
         super().set_likelihood(last_pos, map, strength_weight_list, subject_dist_list)
 
@@ -25,3 +19,9 @@ class Particle(MmParticle):
                         max_corner_weight = corner_weight
 
                 self.likelihood += max_corner_weight
+
+    def walk(self, angle: np.float64, vec: np.ndarray) -> None:
+        if param.ENABLE_PDR_WALK:
+            self._walk(angle + np.random.normal(scale=pf_param.DIRECT_SD), np.linalg.norm(vec) + np.random.normal(scale=param.STRIDE_SD))
+        else:
+            super().random_walk()
