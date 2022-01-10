41004b383a1cc5c467b95cbfa1d0fa687e1c2cca
kazumakano <kazumakano@outlook.jp>
2022-01-10 18:12:32 +0900

    minor modify

--- script/utility.py
+++ script/utility.py
@@ -2,6 +2,12 @@ import math
 import numpy as np
 
 
+def calc_angle(pos_1: np.ndarray, pos_2: np.ndarray, pos_3: np.ndarray) -> float:
+    vec_12: np.ndarray = pos_2 - pos_1
+    vec_13: np.ndarray = pos_3 - pos_1
+
+    return math.degrees(math.acos(np.dot(vec_12, vec_13) / (np.linalg.norm(vec_12) * np.linalg.norm(vec_13))))
+
 # convert angle into (0, 90)
 def conv_to_acute_angle(angle: float) -> float:
     if 0 <= angle <= 90:
@@ -12,9 +18,3 @@ def conv_to_acute_angle(angle: float) -> float:
         return angle - 180
     else:
         return 360 - angle
-
-def calc_angle(pos_1: np.ndarray, pos_2: np.ndarray, pos_3: np.ndarray) -> float:
-    vec_12: np.ndarray = pos_2 - pos_1
-    vec_13: np.ndarray = pos_3 - pos_1
-
-    return math.degrees(math.acos(np.dot(vec_12, vec_13) / (np.linalg.norm(vec_12) * np.linalg.norm(vec_13))))
